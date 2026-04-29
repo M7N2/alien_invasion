67973666739b3383d38a9770303ea0e504a6c721
M7N2 <empty.shell@yahoo.com>
2026-04-29 16:29:46 +0300

    Update ship.py

--- ship.py
+++ ship.py
@@ -6,12 +6,17 @@ class Ship():
         """Инициализирует корабль и задает его начальную позицию."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Загружает изображение корабля и получает прямоугольник.
         self.image = pygame.image.load('images/ship.png').convert_alpha()
         self.rect = self.image.get_rect()
+
         # Каждый новый корабль появляется у нижнего края экрана.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Сохранение вещественной координаты центра корабля.
+        self.x = float(self.rect.x)
 
         # Флаги перемещения.
         self.moving_right = False
@@ -19,10 +24,14 @@ class Ship():
 
     def update(self):
         """Обновляет позицию корабля с учетом флага."""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1    
+        # Обновляет атрибут x, не rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Обновление атрибута rect на основании self.x.
+        self.rect.x = self.x    
 
     def blitme(self):
         """Рисует корабль в текущей позиции."""
