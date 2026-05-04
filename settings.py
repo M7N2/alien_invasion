268f16e76d5912f425e2f23deae9fffd3f330d8b
M7N2 <empty.shell@yahoo.com>
2026-05-04 17:34:02 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -17,3 +17,6 @@ class Settings():
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+        
+        # Настройки звезд.
+        self.number_of_stars = 200
