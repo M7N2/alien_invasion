1e9317de7186df2e6139620bdc6933523836cfe9
M7N2 <empty.shell@yahoo.com>
2026-05-11 20:03:17 +0300

    Update ship.py

--- ship.py
+++ ship.py
@@ -50,3 +50,4 @@ class Ship():
         """Размещает корабль в центре нижней стороны."""
         self.rect.midbottom = self.screen_rect.midbottom
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
