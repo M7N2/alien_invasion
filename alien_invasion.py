f75fe574d9e7c23f71d07636a17c6f4fc0b67981
M7N2 <empty.shell@yahoo.com>
2026-05-11 21:43:38 +0300

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -61,24 +61,28 @@ class AlienInvasion:
                 mouse_pos = pygame.mouse.get_pos()
                 self._check_play_button(mouse_pos)
 
+    def _start_game(self):
+        """Запуск игры."""
+        # Сброс игровой статистики.
+        self.stats.reset_stats()
+        self.stats.game_active = True
+
+        # Очистка списков пришельцев и снарядов.
+        self.aliens.empty()
+        self.bullets.empty()
+
+        # Создание нового флота и размещение корабля в центе.
+        self._create_fleet()
+        self.ship.center_ship()
+
+        # Указатель мыши скрываетя.
+        pygame.mouse.set_visible(False)            
+
     def _check_play_button(self, mouse_pos):
         """Запускает игру при нажатии Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
-            # Сброс игровой статистики.
-            self.stats.reset_stats()
-            self.stats.game_active = True
-
-            # Очистка списков пришельцев и снарядов.
-            self.aliens.empty()
-            self.bullets.empty()
-
-            # Создание нового флота и размещение корабля в центе.
-            self._create_fleet()
-            self.ship.center_ship()
-
-            # Указатель мыши скрываетя.
-            pygame.mouse.set_visible(False)
+            self._start_game()
 
     def _check_keydown_events(self, event):
         """Реагирует на нажатие клавиш."""
@@ -98,7 +102,9 @@ class AlienInvasion:
         elif event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
             sys.exit()
         elif event.key == pygame.K_SPACE:
-            self._fire_bullet()    
+            self._fire_bullet()
+        elif event.key == pygame.K_RETURN:
+            self._start_game() 
 
     def _check_keyup_events(self, event):
         """Реагирует на отпускание клавиш."""
