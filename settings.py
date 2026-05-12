5197c488de8578f264218deb4aad022b78f1fd4b
M7N2 <empty.shell@yahoo.com>
2026-05-12 14:59:42 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ class Settings():
         self.ship_limit = 3
 
         # Параметры снаряда.
-        self.bullet_speed = 1
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (200, 60, 60)
@@ -27,3 +27,20 @@ class Settings():
         self.fleet_drop_speed = 10
         # fleet_direction = 1 обозначает движение вправо, а -1 - влево.
         self.fleet_direction = 1
+
+        # Темп ускорения игры.
+        self.speedup_scale = 1.1
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Инициализирует настройки, изменяющиеся в ходе игры."""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor =  3.0
+        self.alien_speed_factor = 1.0
+
+        # fleet_direction = 1 обозначает движение вправо, а -1 - влево.
+        self.fleet_direction = 1
+
+        
+
+
