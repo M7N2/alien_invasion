8c3c1a0576aeeaed647d5085ac2a57abc9eb2b57
M7N2 <empty.shell@yahoo.com>
2026-05-12 16:46:57 +0300

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -82,6 +82,8 @@ class AlienInvasion:
         """Запускает игру при нажатии Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            # Сброс игровых настроек.
+            self.settings.initialize_dynamic_settings()
             self._start_game()
 
     def _check_keydown_events(self, event):
@@ -142,6 +144,7 @@ class AlienInvasion:
             # Уничтожение существующих снарядов и создание нового флота.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _update_aliens(self):
         """Обновляет позиции всех пришельцев во флоте."""
