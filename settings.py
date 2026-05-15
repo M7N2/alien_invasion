a30e6462fb7521bc2417e87ee2049ce107ce51ea
M7N2 <empty.shell@yahoo.com>
2026-05-15 15:02:01 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -25,6 +25,10 @@ class Settings():
 
         # Темп ускорения игры.
         self.speedup_scale = 1.1
+
+        # Темп роста стоимости прищельца.
+        self.score_scale = 1.5
+
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
@@ -40,11 +44,12 @@ class Settings():
         self.fleet_direction = 1
 
     def increase_speed(self):
-        """Увеличивает насстройки скорости."""
+        """Увеличивает настройки скорости и количество очков."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
-        
+        self.alien_points = int(self.alien_points * self.score_scale)
+
     def set_hard_difficulty(self):
         """Устанавливает настройки для сложного режима."""
         self.ship_speed = 2.5
