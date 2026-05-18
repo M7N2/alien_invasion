c21a9af2eea1531ba209005d48ddb1f3cbe6e005
M7N2 <empty.shell@yahoo.com>
2026-05-18 14:31:54 +0300

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -61,7 +61,7 @@ class Scoreboard():
 
     def prep_level(self):
         """Преобразует уровень в графическое изображение."""
-        level_str = str(self.stats.level)
+        level_str = f"L {self.stats.level}"
         self.level_image = self.font.render(level_str, True,
                         self.text_color, self.settings.bg_color)
 
